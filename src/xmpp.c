5bfc3cbb9899594d85300a37640a6ec2f8f8be00
Tom Wambold <tom5760@gmail.com>
2011-10-13 16:40:40 -0400

    Accept incoming connections and print incoming data.
    Actuallly accept connections instead of just immediately closing them.
    Right now, it just prints each message received.

--- src/xmpp.c
+++ src/xmpp.c
@@ -6,7 +6,10 @@
 
 #include "xmpp.h"
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <sys/types.h>
@@ -15,14 +18,54 @@
 
 #include "event.h"
 
+#define BUFFER_SIZE 2000
+
 static const int SERVER_BACKLOG = 3;
 
-static void xmpp_new_connection(struct event_loop *loop, int fd, void *data) {
-    printf("New connection!\n");
+static char MSG_BUFFER[BUFFER_SIZE];
 
-    int newfd = accept(fd, NULL, NULL);
-    shutdown(newfd, SHUT_RDWR);
-    close(newfd);
+struct client_info {
+    struct sockaddr_in caddr;
+};
+
+static void xmpp_read_client(struct event_loop *loop, int fd, void *data) {
+    struct client_info *info = (struct client_info *)data;
+
+    ssize_t numrecv = recv(fd, MSG_BUFFER, sizeof(MSG_BUFFER), 0);
+
+    if (numrecv == 0 || numrecv == -1) {
+        switch (numrecv) {
+            case 0:
+                printf("%s:%d disconnected\n", inet_ntoa(info->caddr.sin_addr),
+                       info->caddr.sin_port);
+                break;
+            case -1:
+                fprintf(stderr, "Error reading from %s:%d: %s",
+                        inet_ntoa(info->caddr.sin_addr), info->caddr.sin_port,
+                        strerror(errno));
+                break;
+        }
+        event_deregister_callback(loop, fd);
+        close(fd);
+        return;
+    }
+
+    printf("%s:%d(%d bytes): %.*3$s\n", inet_ntoa(info->caddr.sin_addr),
+           info->caddr.sin_port, numrecv, MSG_BUFFER);
+}
+
+static void xmpp_new_connection(struct event_loop *loop, int fd, void *data) {
+    struct client_info *info = malloc(sizeof(struct client_info));
+    if (info == NULL) {
+        fprintf(stderr, "Error allocating connection structure.\n");
+        abort();
+    }
+
+    socklen_t addrlen = sizeof(info->caddr);
+    int clientfd = accept(fd, (struct sockaddr*)&info->caddr, &addrlen);
+    printf("New connection from %s:%d\n", inet_ntoa(info->caddr.sin_addr),
+           info->caddr.sin_port);
+    event_register_callback(loop, clientfd, xmpp_read_client, info);
 }
 
 bool xmpp_init(struct event_loop *loop, struct in_addr addr, uint16_t port) {
