d4c125fb00f7026721f22cd1314b90a5edc11fb1
Tom Wambold <tom5760@gmail.com>
2011-10-18 00:53:44 -0400

    Send initial stream header and features to client.
    Very first step in authentication.  After receiving client stream
    header, send our own, along with the stream features.  Doesn't do anything
    else yet.

--- src/xmpp_auth.c
+++ src/xmpp_auth.c
@@ -6,9 +6,47 @@
 
 #include "xmpp_auth.h"
 
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <expat.h>
+
 #include "log.h"
+#include "xmpp.h"
+
+#define XMLNS_STREAM "http://etherx.jabber.org/streams"
+
+static const char *XML_STREAM = XMLNS_STREAM " stream";
+
+static const char *MSG_STREAM_HEADER =
+    "<stream:stream "
+        "from='localhost' "
+        "id='foobarx' "
+        "version='1.0' "
+        "xml:lang='en' "
+        "xmlns='jabber:client' "
+        "xmlns:stream='http://etherx.jabber.org/streams'>";
+
+static const char *MSG_STREAM_FEATURES_TLS =
+    "<stream:features>"
+        "<starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'>"
+            "<required/>"
+        "</starttls>"
+    "</stream:features>";
+
+static const char *MSG_STREAM_FEATURES_PLAIN =
+    "<stream:features>"
+        "<mechanisms xmlns='urn:ietf:params:xml:ns:xmpp-sasl'>"
+            "<mechanism>PLAIN</mechanism>"
+        "</mechanisms>"
+    "</stream:features>";
 
 void xmpp_auth_init_start(void *data, const char *name, const char **attrs) {
+    struct client_info *info = (struct client_info*)data;
+
     log_info("Element start!");
     printf("<%s", name);
 
@@ -16,6 +54,36 @@ void xmpp_auth_init_start(void *data, const char *name, const char **attrs) {
         printf(" %s=\"%s\"", attrs[i], attrs[i + 1]);
     }
     printf(">\n");
+
+    // name and attrs are guaranteed to be null terminated, so strcmp is OK.
+    check(strcmp(name, XML_STREAM) == 0, "Unexpected message");
+
+    // Send the stream header
+    ssize_t numsent = 0;
+    do {
+        ssize_t newsent = send(info->fd, MSG_STREAM_HEADER + numsent,
+                               strlen(MSG_STREAM_HEADER) - numsent, 0);
+        check(newsent != -1, "Error sending stream header to client");
+        numsent += newsent;
+    } while (numsent < strlen(MSG_STREAM_HEADER));
+
+    // Send the (plain) stream features
+    numsent = 0;
+    do {
+        ssize_t newsent = send(info->fd,
+                               MSG_STREAM_FEATURES_PLAIN + numsent,
+                               strlen(MSG_STREAM_FEATURES_PLAIN) - numsent, 0);
+        check(newsent != -1, "Error sending stream features to client");
+        numsent += newsent;
+    } while (numsent < strlen(MSG_STREAM_FEATURES_PLAIN));
+
+    log_info("Sent stream features to client");
+
+    return;
+
+error:
+    // Going to need to do something different here...
+    XML_StopParser(info->parser, false);
 }
 
 void xmpp_auth_init_end(void *data, const char *name) {
