7b8371a4d9760b8a54babff101798a30521d28b0
Tom Wambold <tom5760@gmail.com>
2011-10-21 18:00:31 -0400

    Remove unnecessary Expat handlers.
    msg_data and msg_end will be handled by other functions.  If we get
    arbitrary message data, then thats an error instead.

--- src/xmpp_im.c
+++ src/xmpp_im.c
@@ -8,35 +8,26 @@
 
 #include <expat.h>
 
+#include "log.h"
 #include "xmpp_common.h"
 
 // Forward declarations
 static void msg_start(void *data, const char *name, const char **attrs);
-static void msg_data(void *data, const char *s, int len);
-static void msg_end(void *data, const char *name);
+
 
 void xmpp_im_set_handlers(XML_Parser parser) {
-    XML_SetElementHandler(parser, msg_start, msg_end);
-    XML_SetCharacterDataHandler(parser, msg_data);
+    XML_SetElementHandler(parser, msg_start, xmpp_error_end);
+    XML_SetCharacterDataHandler(parser, xmpp_error_data);
 }
 
 static void msg_start(void *data, const char *name, const char **attrs) {
-    //struct client_info *info = (struct client_info*)data;
+    struct client_info *info = (struct client_info*)data;
 
     log_info("Message start");
     xmpp_print_start_tag(name, attrs);
+
+
 }
 
-static void msg_data(void *data, const char *s, int len) {
-    //struct client_info *info = (struct client_info*)data;
 
-    log_info("Message data");
-    xmpp_print_data(s, len);
-}
-
-static void msg_end(void *data, const char *name) {
-    //struct client_info *info = (struct client_info*)data;
-
-    log_info("Message end");
-    xmpp_print_end_tag(name);
 }
