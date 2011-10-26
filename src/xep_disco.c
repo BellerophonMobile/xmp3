916cd5c4a2c042789f78f3d3e0b1369521296c07
Tom Wambold <tom5760@gmail.com>
2011-10-26 18:43:10 -0400

    Send not-implemented messages from Disco.
    I'm probably going to remove this for now, since it doesn't seem
    absolutely necessary for basic chat.

--- src/xep_disco.c
+++ src/xep_disco.c
@@ -13,6 +13,25 @@
 #include "xmpp_common.h"
 #include "xmpp_im.h"
 
+// XML String constants
+static const char *MSG_ITEMS_ERR =
+    "<iq type='error' id='%s'>"
+        "<query xmlns='http://jabber.org/protocol/disco#items'/>"
+        "<error type='cancel'>"
+            "<feature-not-implemented "
+                "xmlns='urn:ietf:params:xml:ns:xmpp-stanzas'/>"
+        "</error>"
+    "</iq>";
+
+static const char *MSG_INFO_ERR =
+    "<iq type='error' id='%s'>"
+        "<query xmlns='http://jabber.org/protocol/disco#info'/>"
+        "<error type='cancel'>"
+            "<feature-not-implemented "
+                "xmlns='urn:ietf:params:xml:ns:xmpp-stanzas'/>"
+        "</error>"
+    "</iq>";
+
 // Forward declarations
 static void query_info_end(void *data, const char *name);
 
@@ -38,9 +57,14 @@ error:
 static void query_info_end(void *data, const char *name) {
     struct iq_data *iq_data = (struct iq_data*)data;
     struct client_info *info = iq_data->info;
+    char info_msg[strlen(MSG_INFO_ERR) + strlen(iq_data->attrs.id)];
 
     log_info("Info Query IQ End");
     check(strcmp(name, XMPP_IQ_QUERY_INFO) == 0, "Unexpected IQ");
+
+    snprintf(info_msg, sizeof(info_msg), MSG_ITEMS_ERR, iq_data->attrs.id);
+    check(sendall(info->fd, info_msg, strlen(info_msg)) > 0,
+          "Error sending Disco info query items");
 
     XML_SetEndElementHandler(info->parser, iq_data->end_handler);
     return;
@@ -71,9 +95,14 @@ error:
 static void query_items_end(void *data, const char *name) {
     struct iq_data *iq_data = (struct iq_data*)data;
     struct client_info *info = iq_data->info;
+    char items_msg[strlen(MSG_ITEMS_ERR) + strlen(iq_data->attrs.id)];
 
     log_info("Items Query IQ End");
     check(strcmp(name, XMPP_IQ_QUERY_ITEMS) == 0, "Unexpected IQ");
+
+    snprintf(items_msg, sizeof(items_msg), MSG_ITEMS_ERR, iq_data->attrs.id);
+    check(sendall(info->fd, items_msg, strlen(items_msg)) > 0,
+          "Error sending Disco item query items");
 
     XML_SetEndElementHandler(info->parser, iq_data->end_handler);
     return;
